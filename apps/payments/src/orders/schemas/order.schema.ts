d429234842ec9635d47e6c2c1fd909bad07f16eb
Esteban Ramirez <estebanramirezcc@gmail.com>
2026-03-20 11:10:03 -0500

    add orders projection to payments

---
+++ apps/payments/src/orders/schemas/order.schema.ts
@@ -0,0 +1,34 @@
+import { OrderStatus } from '@org/common';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ versionKey: false })
+class Order {
+  id!: string;
+
+  // Event version from the orders service projection
+  @Prop({ required: true })
+  version!: number;
+
+  @Prop({ type: String, enum: OrderStatus, required: true, default: OrderStatus.Created })
+  status!: OrderStatus;
+
+  @Prop({ required: true })
+  userId!: string;
+
+  @Prop({ required: true })
+  price!: number
+}
+
+const OrderSchema = SchemaFactory.createForClass(Order);
+
+// Hide sensitive/irrelevant fields when sending JSON responses
+OrderSchema.set('toJSON', {
+  transform(_doc, ret: Record<string, any>) {
+    ret.id = ret._id;
+    delete ret._id;
+  },
+});
+
+
+
+export { Order, OrderSchema };
